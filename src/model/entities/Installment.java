87f9827e893c73b68eef9c3e2404b56fc3bb2c1e
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-05-30 12:45:11 -0300

    initial commit

---
+++ src/model/entities/Installment.java
@@ -0,0 +1,47 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Installment {
+
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+	
+	
+	private Date dueDate;
+	private Double amount;
+	
+	public Installment() {
+		
+	}
+
+	public Installment(Date dueDate, Double amount) {
+		this.dueDate = dueDate;
+		this.amount = amount;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return sdf.format(dueDate) +  "-" + String.format("%.2f", amount);
+	}
+
+	
+	
+	
+}
